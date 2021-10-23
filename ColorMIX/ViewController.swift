ed04fb561f7235209580c6296287079b6444e507
Nikita Ryzhkin <timongo@bk.ru>
2021-10-23 18:40:46 +0400

    ColorMIX creation

--- ColorMIX/ViewController.swift
+++ ColorMIX/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var coloredView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+        blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+        redValueLabel.text = String(round(redSlider.value * 100) / 100)
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        redValueLabel.text = String(round(redSlider.value * 100) / 100)
+    }
+    
+    @IBAction func greenSliderAction() {
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        greenValueLabel.text = String(round(greenSlider.value * 100) / 100)
+}
+    
+    @IBAction func blueSliderAction() {
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        blueValueLabel.text = String(round(blueSlider.value * 100) / 100)
+    }
 }
 
